1ae0f1e7f2f568e3dff5a35b5478596d41f55ea2
vkill <vkill.net@gmail.com>
2022-12-07 21:44:12 +0800

    Upgrade deps

--- instagram-basic-display-api/src/types/access_token.rs
+++ instagram-basic-display-api/src/types/access_token.rs
@@ -8,64 +8,19 @@ use core::time::Duration;
 pub const LONG_LIVED_USER_ACCESS_TOKEN_LIFETIME: Duration = Duration::from_secs(3600 * 24 * 60);
 pub const SHORT_LIVED_USER_ACCESS_TOKEN_LIFETIME: Duration = Duration::from_secs(3600);
 
-wrapping_macro::wrapping! {
+wrapping_macro::wrapping_string! {
     #[derive(Debug, Clone, PartialEq, Eq)]
     pub struct LongLivedUserAccessToken(String);
 }
-impl core::fmt::Display for LongLivedUserAccessToken {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        write!(f, "{}", self.0)
-    }
-}
-impl From<&str> for LongLivedUserAccessToken {
-    fn from(s: &str) -> Self {
-        Self(s.into())
-    }
-}
-impl From<&String> for LongLivedUserAccessToken {
-    fn from(s: &String) -> Self {
-        Self(s.into())
-    }
-}
 
-wrapping_macro::wrapping! {
+wrapping_macro::wrapping_string! {
     #[derive(Debug, Clone, PartialEq, Eq)]
     pub struct ShortLivedUserAccessToken(String);
 }
-impl core::fmt::Display for ShortLivedUserAccessToken {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        write!(f, "{}", self.0)
-    }
-}
-impl From<&str> for ShortLivedUserAccessToken {
-    fn from(s: &str) -> Self {
-        Self(s.into())
-    }
-}
-impl From<&String> for ShortLivedUserAccessToken {
-    fn from(s: &String) -> Self {
-        Self(s.into())
-    }
-}
 
-wrapping_macro::wrapping! {
+wrapping_macro::wrapping_string! {
     #[derive(Debug, Clone, PartialEq, Eq)]
     pub struct UserAccessToken(String);
-}
-impl core::fmt::Display for UserAccessToken {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        write!(f, "{}", self.0)
-    }
-}
-impl From<&str> for UserAccessToken {
-    fn from(s: &str) -> Self {
-        Self(s.into())
-    }
-}
-impl From<&String> for UserAccessToken {
-    fn from(s: &String) -> Self {
-        Self(s.into())
-    }
 }
 
 impl From<LongLivedUserAccessToken> for UserAccessToken {
